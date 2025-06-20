18875b4f082c7d3ee9aaeb0eed6988ce5a3fe1e5
Alexandre Menezes <cbittencourt1980@gmail.com>
2025-06-20 16:45:07 +0100

    Add files via upload

---
+++ projeto1/OlaMundo.java
@@ -0,0 +1,27 @@
+package projeto1;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá Mundo");
+		System.out.println("Hello World");
+		System.out.println("Olá Mundinho");
+		String amigo = "Ximporimfola";
+		System.out.println("Olá " + amigo + " Mundão");
+
+		int idade; // Declara uma variável
+		idade = 20; // Atribui valor à variável
+		double altura = 1.75; // Declara e atribui valor à variável simultaneamente
+		char letra = 'A';
+		boolean alunoAtivo = true;
+		String nome = "Carlos Alexandre";
+
+		System.out.println("Nome: " + nome);
+		System.out.println("Idade: " + idade);
+		System.out.println("Caracter: " + letra);
+		System.out.println("Altura: " + altura);
+		System.out.println("É Aluno: " + alunoAtivo);
+	}
+
+}
